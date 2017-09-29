2e85f1cf1cd129fcc9c8a4df3361bf11573059ab
scoder <stefan_ml@behnel.de>
2017-09-29 13:33:31 +0200

    Update travis config to point to Cython at 53327193c9696f3d26320c6cc0c51518f5e6702e.

--- tools/travis/before_install.sh
+++ tools/travis/before_install.sh
@@ -60,7 +60,7 @@ python -m pip install --upgrade pip
 pip install --retries 3 -q wheel flake8 codecov pytest pytest-cov
 # install numpy from PyPI instead of our wheelhouse
 pip install --retries 3 -q wheel numpy
-pip install --retries 3 https://github.com/cython/cython/archive/2835d5bb6c2d646f05e8a1dff46a6b824222fcd8.zip --install-option=--no-cython-compile
+pip install --retries 3 https://github.com/cython/cython/archive/53327193c9696f3d26320c6cc0c51518f5e6702e.zip --install-option=--no-cython-compile
 
 # install wheels
 for requirement in $WHEELBINARIES; do
